f5901d585d740f2bcfb5d8075ef304cdb20c7778
Aleksander Lorenc <thaxsillion@gmail.com>
2023-08-19 23:18:07 +0200

    Update

--- gatherers/zigbeedevicegatherer/init.go
+++ gatherers/zigbeedevicegatherer/init.go
@@ -335,7 +335,7 @@ func (z *ZigbeeDeviceGatherer) gatherFromZHA(databasePath string, nameByIEEE map
 		deviceRow := db.QueryRow(fmt.Sprintf("SELECT %s FROM %s WHERE %s = ?", lastSeen, devicesTable, ieee), deviceIeee)
 		var timestamp float64
 		if err := deviceRow.Scan(&timestamp); err == nil {
-			lastUpdated := time.Unix(int64(timestamp/1000.0), 0)
+			lastUpdated := time.Unix(int64(timestamp), 0)
 			log.Infof("Got time %s", lastUpdated)
 			if lastUpdated.After(device.LastUpdated) {
 				log.Infof("Is after")
