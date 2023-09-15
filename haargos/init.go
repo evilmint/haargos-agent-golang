5f19ae2d03a779281429807656649130944578bf
Aleksander Lorenc <thaxsillion@gmail.com>
2023-09-15 14:42:30 +0200

    Fetch CPU load periodically.

--- haargos/init.go
+++ haargos/init.go
@@ -167,7 +167,7 @@ func (h *Haargos) Run(params RunParams) {
 	if os.Getenv("DEBUG") == "true" {
 		interval = 1 * time.Minute
 	} else {
-		interval = 60 * time.Minute
+		interval = 15 * time.Minute
 	}
 
 	ticker := time.NewTicker(interval)
