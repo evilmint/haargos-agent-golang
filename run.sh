bfc2f1345907157ddfad158494bcc046bf828b86
Aleksander Lorenc <thaxsillion@gmail.com>
2023-11-19 22:40:56 +0100

    Try ARM.

--- run.sh
+++ run.sh
@@ -4,11 +4,18 @@ set -e
 # Configuration
 CONFIG_PATH=/data/options.json
 
-AGENT_TOKEN=$(bashio::config "agent_token")
+declare agent_token
+
+## Get the 'message' key from the user config options.
+agent_token=$(bashio::config 'agent_token')
+
+## Print the message the user supplied, defaults to "Hello World..."
+bashio::log.info "${agent_token:="NO_TOKEN"}"
+
 HA_CONFIG="/config"
 
 # Start Haargos
 
 bashio::log.info "Starting Haargos..."
 
-./haargos run --agent-token "${AGENT_TOKEN}" --ha-config "${HA_CONFIG}"
+./haargos run --agent-token "${agent_token}" --ha-config "${HA_CONFIG}"
