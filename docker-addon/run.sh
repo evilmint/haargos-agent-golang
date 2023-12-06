be5abd931a9d980e9e430e00313e9980bc24c12c
Aleksander Lorenc <thaxsillion@gmail.com>
2023-12-06 19:59:06 +0100

    ZHA support

--- docker-addon/run.sh
+++ docker-addon/run.sh
@@ -1,27 +1,13 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin
 set -e
 
-# Configuration
-CONFIG_PATH=/data/options.json
-declare agent_token
-
-agent_token=$(bashio::config 'agent_token')
-HA_CONFIG="/config/"
-
-if [ -z "${agent_token}" ]; then
-    bashio::log.error "Agent token is not set. Exiting..."
+if [ -z "${HAARGOS_AGENT_TOKEN}" ]; then
+    echo "Agent token is not set. Exiting..."
     exit 1
 fi
 
 # Start Haargos
 
-bashio::log.info "Starting Haargos..."
+echo "Starting Haargos..."
 
-XDEBUG="false"
-# Check for debug mode without causing the script to exit if debug mode is false
-if bashio::config.true 'debug_mode'; then
-    XDEBUG="true"
-    bashio::log.info "Debug mode is enabled."
-fi
-
-DEBUG="${XDEBUG}" HAARGOS_AGENT_TOKEN="${agent_token}" ./haargos run --agent-type addon --zha-path "${HA_CONFIG}zigbee.db" --ha-config "${HA_CONFIG}"
+DEBUG="${DEBUG}" HAARGOS_AGENT_TOKEN="${HAARGOS_AGENT_TOKEN}" ./haargos run --agent-type addon --zha-path "/config/zigbee.db" --ha-config "/config"
