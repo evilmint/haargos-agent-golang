fce2fc7513465e00af90434b6d9665d9bd6529e6
Aleksander Lorenc <thaxsillion@gmail.com>
2024-01-10 12:27:02 +0100

    Notifications - check it works.

--- client/init.go
+++ client/init.go
@@ -6,8 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-
-	"io"
+	"strings"
 
 	"github.com/evilmint/haargos-agent-golang/types"
 	websocketclient "github.com/evilmint/haargos-agent-golang/websocket-client"
@@ -44,24 +43,32 @@ func (c *HaargosClient) sendRequest(method, url string, data interface{}) (*http
 
 	c.Logger.Debugf("Sending %s", string(jsonData))
 
-	var buf bytes.Buffer
-	g := gzip.NewWriter(&buf)
-	if _, err = g.Write(jsonData); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
-	}
-	if err = g.Close(); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
+	hasPayload := strings.ToLower(method) == "put" || strings.ToLower(method) == "post"
+
+	var buf *bytes.Buffer = nil
+
+	if hasPayload {
+		g := gzip.NewWriter(buf)
+		if _, err = g.Write(jsonData); err != nil {
+			c.Logger.Error(err)
+			return nil, fmt.Errorf("error compressing JSON: %v", err)
+		}
+		if err = g.Close(); err != nil {
+			c.Logger.Error(err)
+			return nil, fmt.Errorf("error compressing JSON: %v", err)
+		}
 	}
 
-	req, err := http.NewRequest(method, c.BaseURL+url, &buf)
+	req, err := http.NewRequest(method, c.BaseURL+url, buf)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %v", err)
 	}
 
-	req.Header.Set("Content-Encoding", "gzip")
-	req.Header.Set("Content-Type", "application/json")
+	if hasPayload {
+		req.Header.Set("Content-Encoding", "gzip")
+		req.Header.Set("Content-Type", "application/json")
+	}
+
 	req.Header.Set("x-agent-token", c.AgentToken)
 
 	client := &http.Client{}
@@ -75,19 +82,9 @@ func (c *HaargosClient) sendRequest(method, url string, data interface{}) (*http
 }
 
 func (c *HaargosClient) FetchAgentConfig() (*AgentConfig, error) {
-	url := c.BaseURL + "agent-config"
-
-	req, err := http.NewRequest("GET", url, nil)
+	resp, err := c.sendRequest("GET", "agent-config", nil)
 	if err != nil {
-		return nil, fmt.Errorf("error creating request: %v", err)
-	}
-
-	req.Header.Set("x-agent-token", c.AgentToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error sending request: %v", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
@@ -95,13 +92,8 @@ func (c *HaargosClient) FetchAgentConfig() (*AgentConfig, error) {
 		return nil, fmt.Errorf("received non-OK response status: %s", resp.Status)
 	}
 
-	bodyBytes, err := io.ReadAll(io.Reader(resp.Body))
-	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %v", err)
-	}
-
 	var config AgentConfigResponse
-	if err := json.Unmarshal(bodyBytes, &config); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&config); err != nil {
 		return nil, fmt.Errorf("error unmarshaling response: %v", err)
 	}
 
@@ -113,121 +105,14 @@ type NotificationRequest struct {
 }
 
 func (c *HaargosClient) SendNotifications(notifications []websocketclient.WSAPINotificationDetails) (*http.Response, error) {
-	url := c.BaseURL + "installations/notifications"
-
-	jsonData, err := json.Marshal(NotificationRequest{Notifications: notifications})
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling JSON: %v", err)
-	}
-
-	c.Logger.Debugf("Sending %s", string(jsonData))
-
-	var buf bytes.Buffer
-	g := gzip.NewWriter(&buf)
-	if _, err = g.Write(jsonData); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
-	}
-	if err = g.Close(); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
-	}
-
-	req, err := http.NewRequest("PUT", url, &buf)
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %v", err)
-	}
-
-	req.Header.Set("Content-Encoding", "gzip")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("x-agent-token", c.AgentToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error sending request: %v", err)
-	}
-
-	fmt.Printf("Response status: %s\n", resp.Status)
-	return resp, nil
+	requestData := NotificationRequest{Notifications: notifications}
+	return c.sendRequest("PUT", "installations/notifications", requestData)
 }
 
 func (c *HaargosClient) SendLogs(logs types.Logs) (*http.Response, error) {
-	url := c.BaseURL + "installations/logs"
-
-	jsonData, err := json.Marshal(logs)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling JSON: %v", err)
-	}
-
-	c.Logger.Debugf("Sending %s", string(jsonData))
-
-	var buf bytes.Buffer
-	g := gzip.NewWriter(&buf)
-	if _, err = g.Write(jsonData); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
-	}
-	if err = g.Close(); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
-	}
-
-	req, err := http.NewRequest("PUT", url, &buf)
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %v", err)
-	}
-
-	req.Header.Set("Content-Encoding", "gzip")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("x-agent-token", c.AgentToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error sending request: %v", err)
-	}
-
-	fmt.Printf("Response status: %s\n", resp.Status)
-	return resp, nil
+	return c.sendRequest("PUT", "installations/logs", logs)
 }
 
 func (c *HaargosClient) SendObservation(observation types.Observation) (*http.Response, error) {
-	url := c.BaseURL + "observations"
-
-	jsonData, err := json.Marshal(observation)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling JSON: %v", err)
-	}
-
-	c.Logger.Debugf("Sending %s", string(jsonData))
-
-	var buf bytes.Buffer
-	g := gzip.NewWriter(&buf)
-	if _, err = g.Write(jsonData); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
-	}
-	if err = g.Close(); err != nil {
-		c.Logger.Error(err)
-		return nil, fmt.Errorf("error compressing JSON: %v", err)
-	}
-
-	req, err := http.NewRequest("POST", url, &buf)
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %v", err)
-	}
-
-	req.Header.Set("Content-Encoding", "gzip")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("x-agent-token", c.AgentToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error sending request: %v", err)
-	}
-
-	fmt.Printf("Response status: %s\n", resp.Status)
-	return resp, nil
+	return c.sendRequest("POST", "observations", observation)
 }
